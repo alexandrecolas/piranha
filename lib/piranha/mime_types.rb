208c56e6607f7fa664affd756baf251a5869cf0b
Alexandre Colas <colasalexandre@gmail.com>
2016-06-15 11:12:16 +0200

    Add "application/excel" types

--- lib/piranha/mime_types.rb
+++ lib/piranha/mime_types.rb
@@ -8,6 +8,7 @@ module Piranha
       "application/msword",
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
       "application/vnd.ms-excel",
+      "application/excel",
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
       "application/vnd.ms-powerpoint",
       "application/vnd.openxmlformats-officedocument.presentationml.presentation",
